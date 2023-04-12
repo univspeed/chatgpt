2ab02bfb0aa2a655fd00fd75040fcdbb4166f74d
liuyutang <liuyutangabc@163.com>
2023-04-12 09:50:10 +0800

    init project

---
+++ src/main/java/com/univspeed/chat/tpchatgpt/TpchatgptApplication.java
@@ -0,0 +1,13 @@
+package com.univspeed.chat.tpchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpchatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpchatgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univspeed/chat/tpchatgpt/TpchatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univspeed.chat.tpchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpchatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
